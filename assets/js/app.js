01304a168ec938ec52c27e6dbc493f6a61dd156f
JeppeFH <jeppefinch@hotmail.dk>
2024-06-18 08:26:21 +0200

    updatering af kommentar

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,25 +1,36 @@
 let form = document.querySelector(".form");
 
 form.addEventListener("submit", function (event) {
+  /*tilføjer event listener til form. Lytter specifikt efter "submit" event, 
+  hvilket sker, når brugeren forsøger at sende formularen enten ved at trykke 
+  på en submit-knap*/
   event.preventDefault;
+  /*For at , kontrolleres der om brugeren har udfyldt 
+  alle nødvendige felter korrekt.*/
 });
 
+// Find DOM-elementer
 let submitInput = document.querySelector("#submitIput");
 let nameInput = document.querySelector("#nameInput");
 let birthInput = document.querySelector("#birthIput");
 let mailInput = document.querySelector("#mailIput");
 
+// Initialiser variabler
 let isValid = true;
 let errorMessage = [];
 
+// Valider navn
 if (nameInput.value.trim() === "") {
+  /*For hvert inputfelt, tjekkes om værdien (efter at have fjernet 
+  eventuelle whitespace med trim()) er tom ("").*/
   isValid = false;
   nameInput.classList.add("Error");
   errorMessage.push("Skriv dit navn");
 } else {
-  nameInput.classList.remove("error");
+  nameInput.classList.remove("Error");
 }
 
+// Valider fødselsdato
 if (birthInput.value.trim() === "") {
   isValid = false;
   birthInput.classList.add("Error");
@@ -28,6 +39,7 @@ if (birthInput.value.trim() === "") {
   birthInput.classList.remove("Error");
 }
 
+// Valider E-mail
 if (mailInput.value.trim() === "") {
   isValid = false;
   mailInput.classList.add("Error");
@@ -36,6 +48,8 @@ if (mailInput.value.trim() === "") {
   mailInput.classList.remove("Error");
 }
 
+/*if statement som siger at hvis alle felter er udfyldt korrekt,
+send formularen*/
 if (isValid) {
   form.submit();
 }
@@ -44,5 +58,4 @@ if (isValid) {
 
 //if (nameInput.value.trim() === "")
 // value = angiver værdien
-// trim() = Betyder at man ikke vil kunne bruge spaceknappen til at lave mellemrum for at validere.
 // === "" {isValid = false;} = Betyder at den er tom, hvilket betyder at den er false.
